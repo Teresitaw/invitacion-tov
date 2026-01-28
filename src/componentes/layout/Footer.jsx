dbe4f45d5c44817fda048e8121dd4930edb6a24b
Teresitaw <tere6_6eret@hotmail.com>
2026-01-28 17:22:06 -0600

    Actualizacion Responsive

--- src/componentes/layout/Footer.jsx
+++ src/componentes/layout/Footer.jsx
@@ -3,12 +3,12 @@ import "./Footer.css";
 const Footer = () => {
 	return (
 		<>
-			<footer class="footer">
-				<a class="logo" href="https://teresitaw.github.io/links/" target="_blank">
+			<footer className="footer">
+				<a className="logo" href="https://teresitaw.github.io/links/" target="_blank">
 					<img src="/assets/logo.jpg" alt="logo de teresitaw" />
 					<p>© 2026 Teresitaw - Desarrolladora web & Freelancer</p>
 				</a>
-				<nav class="links">
+				<nav className="links">
 					<a href="#">Términos y Condiciones</a>
 					<a href="#">Política de Privacidad</a>
 				</nav>
